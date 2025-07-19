0eb2c7db4f597f2e4e2e77ec073d84627f7fc5a4
@Sameera1994 <lakshithasameera89@gmail.com>
2025-07-19 23:21:00 +0530

    update composer

--- .github/workflows/vendor/composer/installed.php
+++ .github/workflows/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'sameera1994/teflo',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '323fe0265a0ddbdf3de976af62c0e12ac178945e',
+        'reference' => '48d02fc5ae99f988fefa1237aa2df790892d90fb',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'sameera1994/teflo' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '323fe0265a0ddbdf3de976af62c0e12ac178945e',
+            'reference' => '48d02fc5ae99f988fefa1237aa2df790892d90fb',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
