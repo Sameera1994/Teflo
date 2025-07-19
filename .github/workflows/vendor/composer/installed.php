48d02fc5ae99f988fefa1237aa2df790892d90fb
@Sameera1994 <lakshithasameera89@gmail.com>
2025-07-19 23:12:24 +0530

    add composer files

---
+++ .github/workflows/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sameera1994/teflo',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '323fe0265a0ddbdf3de976af62c0e12ac178945e',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sameera1994/teflo' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '323fe0265a0ddbdf3de976af62c0e12ac178945e',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
